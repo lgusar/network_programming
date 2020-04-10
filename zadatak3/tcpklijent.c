9d0429b25acb7a817b6256d252a2026d97c0fe08
Lovre Gusar <lg50712@fer.hr>
2020-04-10 02:50:31 +0200

    can send files between the server and client

--- zadatak3/tcpklijent.c
+++ zadatak3/tcpklijent.c
@@ -17,3 +17,99 @@
     kad primi datoteku/server prekine konekciju, tcpklijent završava s radom s 
     statusom koji primi od servera (0x00, 0x01, 0x02, 0x03)
 */
+
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "wrapper_functions.h"
+
+#define DEFAULT_SERVER "127.0.0.1"
+#define DEFAULT_PORT 1234
+#define MAX_DATA 256
+
+
+void usage()
+{
+    fprintf(stderr, "Usage: ./tcpklijent [-s server] [-p port] [-c] filename\n");
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv)
+{
+    if(argc < 2){
+        usage();
+    }
+    
+    char *server = DEFAULT_SERVER;
+    int port = DEFAULT_PORT;
+
+    char ch;
+    int cflag = 0;
+
+    while((ch = getopt(argc, argv, "s:p:c")) != -1){
+        switch(ch){
+            case 's':
+                server = optarg;
+                break;
+            case 'p':
+                port = atoi(optarg);
+                break;
+            case 'c':
+                cflag = 1;
+                break;
+            default:
+                usage();
+        }
+    }
+
+    char *filename = argv[argc-1];
+    FILE *fp;
+    int offset = 0;
+
+    fp = fopen(filename, "a");
+
+    if(cflag){
+        struct stat st;
+        stat(filename, &st);
+        offset = st.st_size;
+    }
+
+    offset = htonl(offset);
+
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    socklen_t sin_size;
+    struct addrinfo hints, *res;
+    char buf[MAX_DATA];
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_flags |= AI_CANONNAME;
+
+    w_getaddrinfo(server, NULL, &hints, &res);
+
+    sockfd = w_socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr;
+    memset(serv_addr.sin_zero, '\0', sizeof serv_addr.sin_zero);
+
+    w_connect(sockfd, (struct sockaddr *)&serv_addr, sizeof serv_addr);
+    
+    w_send(sockfd, &offset, 4, 0);
+    w_send(sockfd, filename, strlen(filename), 0);
+    
+    w_recv(sockfd, buf, MAX_DATA, 0);
+    int status = atoi(buf);
+    
+    while(w_recv(sockfd, buf, MAX_DATA, 0) > 0){
+        fprintf(fp, "%s", buf);
+    }
+
+    fclose(fp);
+    close(sockfd);
+    freeaddrinfo(res);
+    return status;
+}
--- zadatak3/tcpserver.c
+++ zadatak3/tcpserver.c
@@ -23,3 +23,92 @@
     poddirektorija trenutnog direktorija
     ako u zahtjevu sadrži znak / ili traži nepostojeću datoteku -> 0x01
 */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "wrapper_functions.h"
+
+#define DEFAULT_PORT 1234
+#define BACKLOG 10
+#define MAXDATA 256
+
+void usage()
+{
+    fprintf(stderr, "Usage: ./tcpserver [-p port]\n");
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char **argv)
+{
+    if(argc > 2){
+        usage();
+    }
+
+    int port = DEFAULT_PORT;
+
+    char ch;
+
+    while((ch = getopt(argc, argv, "p:")) != -1){
+        switch(ch){
+            case 'p':
+                port = atoi(optarg);
+                break;
+            default:
+                usage();
+        }
+    }
+
+    int servfd, clifd;
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in cli_addr;
+    socklen_t sin_size;
+    char buf[MAXDATA];
+    char filename[MAXDATA];
+    int offset;
+    
+    servfd = w_socket(AF_INET, SOCK_STREAM, 0);
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    memset(serv_addr.sin_zero, '\0', sizeof serv_addr.sin_zero);
+
+    w_bind(servfd, (struct sockaddr *)&serv_addr, sizeof serv_addr);
+
+    w_listen(servfd, BACKLOG);
+
+    while(1){
+        sin_size = sizeof cli_addr;
+
+        clifd = w_accept(servfd, (struct sockaddr *)&cli_addr, &sin_size);
+           
+        printf("Client connected\n");
+
+        w_recv(clifd, &offset, 4, 0);
+        offset = ntohl(offset);
+        
+        printf("Offset: %d\n", offset);
+
+        w_recv(clifd, filename, MAXDATA, 0);
+        
+        printf("Filename: %s\n", filename);
+
+        FILE *fp;
+        fp = fopen(filename, "r");
+
+        fseek(fp, offset, SEEK_SET);
+
+        while(fgets(buf, MAXDATA, fp)){
+            w_send(clifd, buf, MAXDATA, 0);     
+        }
+
+        close(clifd);
+    }
+
+    close(servfd);
+
+    return 0;
+     
+
+}
