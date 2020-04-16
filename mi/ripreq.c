6294fce5773937d5cb9c323a713aa48ef231ef8b
Lovre Gusar <lg50712@fer.hr>
2020-04-16 12:50:20 +0200

    dodano slanje paketa na preusmjeritelj

--- mi/ripreq.c
+++ mi/ripreq.c
@@ -6,6 +6,7 @@
 
 #define MAX_ENTRY 25
 #define PORT 520
+#define MAX_DATA 504
 
 
 struct rip_entry
@@ -49,19 +50,27 @@ int main(int argc, char **argv)
     }
     
     char *ip_address = argv[1];
+    int port = htons(PORT);
 
     int sockfd = w_socket(AF_INET, SOCK_DGRAM, 0);
     
     struct rip_packet packet;
     memset(&packet, 0, sizeof(packet));
     set_rip_packet(&packet);
+    
+    struct sockaddr_in serv_addr;
+    struct addrinfo hints, *res;
 
-    printf("%d\n", packet.entries[0].metric);
-    printf("%x\n", packet.must_be_zero);
-    printf("%x\n", packet.command);
-    printf("%x\n", packet.version);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    
+    w_getaddrinfo(ip_address, NULL, &hints, &res);
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = port;
+    serv_addr.sin_addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr;
+
+    w_sendto(sockfd, &packet, sizeof(packet), 0, (const struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
     return 0;
-
-
 }
