35b11c4697f3f90ff5dc94bda4727d78c7d2c7c3
Lovre Gusar <lovreg79@gmail.com>
2020-05-27 13:26:54 +0200

    parsiranje url-a

--- zi/httpget.c
+++ zi/httpget.c
@@ -18,6 +18,7 @@
 #include "wrapper_functions.h"
 
 #define HTTP 80
+#define MAXBUFFER 8192
 
 
 void usage(){
@@ -25,34 +26,68 @@ void usage(){
     exit(1);
 }
 
+char *get_address(char *url){
+    char *delim = "/";
+    char *tmp;
+
+    strcpy(tmp, url);
+    
+    char *token;
+    token = strtok(tmp, delim);
+    token = strtok(NULL, delim);
+    token = strtok(NULL, delim);
+
+    return token;
+}
+
+char *get_filepath(char *url){
+    char *delim = "/";
+    char *tmp;
+
+    strcpy(tmp, url);
+    
+    char *token;
+    token = strtok(tmp, delim);
+    token = strtok(NULL, delim);
+    token = strtok(NULL, delim);
+    token = strtok(NULL, delim);
+
+    return token;
+}
+
 void get_file(char *url, char *log){
-    char *address, *file;
+    char *address = get_address(url);
+    char *filepath = get_filepath(url);
 
-    sscanf(url, "http://%99s/%99s", address, file);
+    printf("%s %s\n", address, filepath);
 
-    printf("%s %s\n", address, file);
+    // char *getpacket;
+    // sprintf(getpacket, "GET %s HTTP/1.1\nHost: %s\nConnection: close\n\n", file, address);
+    // char buffer[MAXBUFFER];
 
-    int port = HTTP;
+    // int port = HTTP;
     
-    int sock = w_socket(AF_INET, SOCK_STREAM, 0);
+    // int sock = w_socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in addr;
-    struct addrinfo hints, *res;
+    // struct sockaddr_in addr;
+    // struct addrinfo hints, *res;
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
+    // memset(&hints, 0, sizeof hints);
+    // hints.ai_family = AF_INET;
 
-    w_getaddrinfo(url, NULL, &hints, &res);
+    // w_getaddrinfo(address, NULL, &hints, &res);
     
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr;
+    // addr.sin_family = AF_INET;
+    // addr.sin_port = htons(port);
+    // addr.sin_addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr;
 
-    w_connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+    // w_connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+
+    // w_send(sock, getpacket, strlen(getpacket), 0);
 
 
 
-    close(sock);
+    // close(sock);
 }
 
 int main(int argc, char **argv){
