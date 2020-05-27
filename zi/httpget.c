cef6adb2c73ede9ea368643674fcad0463392a6c
Lovre Gusar <lovreg79@gmail.com>
2020-05-27 12:58:14 +0200

    testiranje parsiranja url-a u adresu i filename

--- zi/httpget.c
+++ zi/httpget.c
@@ -15,8 +15,9 @@
 #include <sys/time.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 #include "wrapper_functions.h"
+
+#define HTTP 80
 
 
 void usage(){
@@ -24,10 +25,32 @@ void usage(){
     exit(1);
 }
 
-void get_file(char *url){
+void get_file(char *url, char *log){
+    char *address, *file;
+
+    sscanf(url, "http://%99/%99", address, file);
+
+    printf("%s %s\n", address, file);
+
+    int port = HTTP;
+    
     int sock = w_socket(AF_INET, SOCK_STREAM, 0);
 
+    struct sockaddr_in addr;
+    struct addrinfo hints, *res;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+
+    w_getaddrinfo(url, NULL, &hints, &res);
     
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr;
+
+    w_connect(sock, (struct sockaddr *)&addr, sizeof(addr));
+
+
 
     close(sock);
 }
@@ -46,6 +69,8 @@ int main(int argc, char **argv){
     char *p1 = argv[2];
     char *p2 = argv[4];
     char *p3 = argv[6];
+
+    get_file(url1, "log1");
 
     return 0;
 }
